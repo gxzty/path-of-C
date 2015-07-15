313df4536ef063e4026c5e78f4fee78a113a46d2
Zty <361222163@qq.com>
2015-07-15 14:40:08 +0800

    update name &rebuild 英雄库(未实现) with Linked List

---
+++ Unit_1/数据结构与算法/英雄库(链表重构版,未完成).c
@@ -0,0 +1,52 @@
+﻿#include<stdio.h>
+#include "GetLevel.c"//GetLevel.c是经验需求表
+
+
+//定义一个结构体
+typedef struct Hero{
+    char Name[20];
+    int exp;
+	struct hero * next;
+}hero;
+
+hero *CreatHero()
+{
+	int NameNumber;
+	return NameNumber;
+}
+
+
+
+//根据输入编号，返回该编号对应的英雄名称
+char GetName(int NameNumber){
+	
+    /*char Name;
+    //输入错误的编号，则提示重新输入。
+    if (NameNumber <=0 || NameNumber >=3){
+    printf("Wrong NameNumber,please reinput (1-2):\n");
+    scanf("%d",&NameNumber);
+    GetName(NameNumber);
+    }
+    else{
+	switch (NameNumber){
+        case 1: Name = "Slark";break;
+        case 2: Name = "Norton";break;
+        }
+    }
+	*/
+    return Name;
+}
+
+
+int main(){
+    char hero,Name;
+    int NameNumber,Exp = 0,Hp,Atk,Level;
+    printf("Please input 编号和经验:\n");
+    scanf("%d,%d",&NameNumber,&Exp);
+    Name = GetName(NameNumber);
+    Level = GetLevel(Exp);
+    struct Hero Slark = {"Slark",100+(10*Level),13+(2*Level)};
+    //hero Norton = {"Norton",150+(15*Level),8+(1*Level)};
+    printf("Hero Name:%s\nLevel:%d\nHp:%d\nAttack:%d\n",Name,Level,Slark.Hp,Slark.Atk);
+    return 0;
+}
