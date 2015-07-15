c8fcf830d51f8998fc72439011e772da76d05bde
Zty <361222163@qq.com>
2015-07-15 14:22:27 +0800

    初识静态链表

---
+++ Unit_1/数据结构与算法/静态链表.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+
+struct people
+{
+	int id;
+	int age;
+    struct people *next;
+};
+
+struct people * Creat()
+{
+	struct people * head;
+	struct people * p1,* p2;
+	int n=0;
+	p1=p2=(struct people*)malloc(sizeof(struct people));
+	printf("input id and age:\n");
+	scanf("%d,%d",&p1->id,&p1->age);//scanf是格式化输入语句，输入数据方式要按照"1,2"的方式来输入！记得逗号！！
+	while (p1->id != 0){
+		n++;
+		if (n==1){
+			head = p1;
+		}
+		else p2->next = p1;
+        p2 = p1;
+		p1 = (struct people*)malloc(sizeof(struct people));
+		scanf("%d,%d",&p1->id,&p1->age);
+	}
+	p2->next =NULL;
+	return (head);
+}
+
+int main(int argc, char *argv[])
+{
+	struct people * p;
+    p = Creat();
+	printf("%d,%d\n",p->id,p->age);
+	return 0;
+}
