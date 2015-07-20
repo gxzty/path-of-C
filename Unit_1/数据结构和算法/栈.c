1c081a5a78e11c00cda7be127055ee6c85482a40
Zty <361222163@qq.com>
2015-07-20 12:31:16 +0800

    Stack!

--- Unit_1/数据结构和算法/栈.c
+++ Unit_1/数据结构和算法/栈.c
@@ -1,19 +1,17 @@
-#include <stdio.h>
 #include <malloc.h>
+#include "define.c"
 #define STACK_INIT_SIZE 100  //存储空间初始分配量
 #define STACK_INCREMENT 10   //存储空间分配增量
-typedef struct Hero{
-    char Name[20];
-    int Exp,Hp,Atk;
-	struct Hero *next;
-}hero;
+
+#define SElemType int
+
 
 //创建一个栈的数据类型
 typedef struct {
-	hero *base;//栈底指针，构造之前和销毁之后均为NULL
-	hero *top;//栈顶指针
-	int stacksize;//栈空间，以元素为单位
-}sqStack;
+	SElemType *base;//栈底指针，构造之前和销毁之后均为NULL
+	SElemType *top;//栈顶指针
+	int stacksize;//栈空间大小，以元素为单位
+}SqStack;
 
 /*top指针初始和base一样，都指向栈底（此时两者均为NULL），增加一个元素，base保持不变，top增加1
 反之，减少一个元素，base保持不变，top减少1
@@ -24,17 +22,48 @@ typedef struct {
 
 */
 
-int InitStack(){
-	sqStack *S;
+Status InitStack(SqStack S){
     //构造一个空栈
-    S->base = (hero *)malloc(STACK_INIT_SIZE * sizeof(hero));
+    S.base = (SElemType *)malloc(STACK_INIT_SIZE * sizeof(SElemType));
+	if ( !S.base ) exit (OVERFLOW);    //存储分配失败 返回-2
+	S.top = S.base;
+	S.stacksize = STACK_INIT_SIZE;
+	return OK;
+}//InitStack
+
+Status GetTop(SqStack S,SElemType *e){
+    //若栈不空，则用e返回S的栈顶元素，并返回OK；否则返回ERROR
+    if (S.top == S.base) return FALSE;
+	e = (S.top - 1);
+	return OK;
+}//GetTop
+
+Status Push(SqStack S,SElemType e){
+	//插入元素e为新的栈顶元素
+}
+
+Status Pop (SqStack S,SElemType e){
+    //若栈不空，则删除S的栈顶元素，用e返回其值，并返回OK；否则返回ERROR
+}
+
+Status DestroyStack(SqStack *S){
+    //销毁栈S,S不再存在
+}
+
+Status ClearStack(SqStack *S){
+    //把S置为空栈
+}
+
+Status StackEmpty(SqStack *S){
+    //若栈S为空栈，则返回TRUE，否则返回FALSE
+}
+
+int StackLength(SqStack *S){
+    //返回S的元素个数，即栈的长度，栈的规模
 }
 
 
-
-
-int main(int argc, char *argv[])
-{
-	
-	return 0;
+Status StackTraverse(SqStack S,Status( * visit)());
+    //从栈底到栈顶依次对栈中每个元素调用函数visit()，一旦visit()失败，则操作失败
+void main (){
 }
