38bf6855c78925b8669ff6c900bfc3eb2e986f1c
Zty <361222163@qq.com>
2015-07-25 12:36:56 +0800

    模块化

--- Unit_0/二分插数.c
+++ Unit_0/二分插数.c
@@ -2,19 +2,18 @@
 #define N 5
 #define INSERT 0
 //特殊INSERT 无外乎大于最大值或者小于最小值
-int main(int argc, char *argv[])
+//重视模块化
+int binary_insert(int *a, int len , int insert)
 {
-    int count = N;  // 现存了N个数
-	int a[6] = {1, 3, 5, 7, 9 }; //a[0]~a[4],a[5]为INSERT预留
-	int b=INSERT;
-	int i,j,k;
-    //如果i小于count,比较b和a[i]谁大，若i=count，则执行a[i]=b;count++;
-	//若b <= a[i]，a[i]后所有元素后移一位，a[i]=b，同时终止for循环
-	//若a[i] < b，执行i++，比较b和a[i+1]
-	int lo = 0,hi = N-1 , mi = (lo+hi)/2;
+	int j,k;
+    //如果j小于count,比较b和a[j]谁大，若j=count，则执行a[j]=b;count++;
+	//若b <= a[j]，a[j]后所有元素后移一位，a[j]=b，同时终止for循环
+	//若a[j] < b，执行j++，比较b和a[j+1]
+	int lo = 0,hi = len-1 , mi = (lo+hi)/2 ;
+	int b = insert , count = len;
     if (a[hi] <= b) {
-		//若b不小于a[]中最大值a[hi]，则直接将b置入a[N]中
-		a[N]=b;
+		//若b不小于a[]中最大值a[hi]，则直接将b置入a[len]中
+		a[len]=b;
 	    count++;
 	}
     else if (b <= a[lo]){
@@ -43,4 +42,11 @@ int main(int argc, char *argv[])
 	}
 	printf("\n%d\n",count);
 	return 0;
+}
+
+int main(){
+	int a[N+1] = {1, 3, 5, 7, 9 }; //a[0]~a[4],a[5]为INSERT预留
+    int count = N;  // 现存了N个数
+	int b=INSERT;
+    binary_insert(a,count,b);
 }
