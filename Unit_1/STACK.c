d0057733399a07c24a59ff4a101b9f41ef083bde
Zty <361222163@qq.com>
2015-07-28 23:40:50 +0800

    Hello Stack.

--- Unit_1/STACK.c
+++ Unit_1/STACK.c
@@ -23,9 +23,24 @@ typedef struct stack{
 	|        |base & top        |  A   |base                 |    A  |base   
 */
 
+//功能列表
+SqStack InitStack(SqStack *S);     //构造一个空栈，需要用栈接收，如SqStack S; S = InitStack(&S);
+Status EmptyStack(SqStack S);     //检测是否为空栈，若为空，返回1，不为空，返回0
+Status Push(SqStack *S,SElemType e);
+int LengthStack(SqStack S);
+Status GetTop(SqStack S,SElemType *e);
+Status Pop (SqStack *S,SElemType *e);
+void BTPrintStack(SqStack S);       //遍历栈，从栈底base到栈顶top
+void TBPrintStack(SqStack S);       //遍历栈，从栈顶top到栈底base
+Status CleanStack(SqStack *S);
+Status DestroyStack(SqStack *S);
+
+
+//具体构造
 SqStack InitStack(SqStack *S){
     //构造一个空栈
-    if(!(S = (SqStack *)malloc(sizeof(SqStack)))) exit (OVERFLOW);    //存储分配失败 返回-2
+	S = (SqStack *)malloc(sizeof(SqStack));
+    if(!(S)) exit (OVERFLOW);    //存储分配失败 返回-2
 	S->base = (SElemType *)malloc(STACK_INIT_SIZE * sizeof(SElemType));
 	S->top = S->base;
 	S->stacksize = STACK_INIT_SIZE;
@@ -33,54 +48,115 @@ SqStack InitStack(SqStack *S){
 	return *S;
 }//InitStack
 
+Status EmptyStack(SqStack S){
+    if (S.top == S.base){
+		return OK;
+    }
+	else return FALSE;
+}
+
+Status Push(SqStack *S,SElemType e){
+	//插入元素e为新的栈顶元素
+	if (S->stacksize <= S->top - S->base){
+        S->base = (SElemType *)realloc(S->base , 
+		                (S->stacksize+STACKINCREMENT)*sizeof(SElemType));
+        if (!S->base) {
+	    	exit (OVERFLOW);
+    	}
+        S->top = S->base + S->stacksize;
+        S->stacksize += STACKINCREMENT;
+	}
+	*S->top = e;
+	S->top++;
+	return OK;
+}//Push
+
+Status GetTop(SqStack S,SElemType *e){
+    //若栈不空，则用e返回S的栈顶元素，并返回OK；否则返回ERROR
+    if (EmptyStack(S)) return FALSE;
+    *e = *(S.top - 1);
+	return OK;
+}//GetTop
+
+Status Pop (SqStack *S,SElemType *e){
+    //若栈不空，则删除S的栈顶元素，用e返回其值，并返回OK；否则返回ERROR
+    if (EmptyStack(*S)){
+		return ERROR;
+	}
+	--S->top;
+	*e =  *S->top;
+	return OK;
+}//Pop
+
+void BTPrintStack(SqStack S){
+	//遍历栈，从栈底base到栈顶top
+    SElemType *elem = S.base;
+	int i = 1;
+	while (elem != S.top){
+		printf("elem %d = %d\n",i++,*elem++);
+	}
+}
+
+void TBPrintStack(SqStack S){
+	//遍历栈，从栈顶top到栈底base
+    SElemType *elem = --S.top;
+	int i = S.top - S.base + 1;
+	while (elem >= S.base){
+		printf("elem %d = %d\n",i--,*elem--);
+	}
+}
+
+int LengthStack(SqStack S){
+    return S.top - S.base;
+}
+
+Status CleanStack(SqStack *S){
+	SElemType *elem;
+	elem = S->base;
+    while (elem != S->top){
+		*elem = 0;
+		elem++;
+    }
+}
+
 Status DestroyStack(SqStack *S){
 	//若栈存在，销毁返回OK;否则返回ERROR
     if (S){
 		free(S->base);
+		S->top = S->base;
+		S->stacksize = 0;
 		free(S);
 		return OK;
 	}
 	else return ERROR;
 }//DestroyStack
 
-Status GetTop(SqStack S,SElemType *e){
-    //若栈不空，则用e返回S的栈顶元素，并返回OK；否则返回ERROR
-    if (S.top == S.base) return FALSE;
-    e = (S.top - 1);
-	return OK;
-}//GetTop
-
-Status Push(SqStack *S,SElemType e){
-	//插入元素e为新的栈顶元素
-	if (S->stacksize <= S->top - S->base){
-    S->base = (SElemType *)realloc(S->base , 
-		                (S->stacksize+STACKINCREMENT)*sizeof(SElemType));
-	if (!S->base) exit (OVERFLOW);
-	S->top = S->base + S->stacksize;
-	S->stacksize += STACKINCREMENT;
-	}
-	*S->top++ = e;
-	printf("e%d\n",e);
-	return OK;
-}//Push
-
-Status Pop (SqStack *S,SElemType *e){
-    //若栈不空，则删除S的栈顶元素，用e返回其值，并返回OK；否则返回ERROR
-    if (S->top == S->base); return ERROR;
-	e =  (S->top--);
-	printf("e%d\n",e);
-	return OK;
-}//Pop
-
 int main(){
+	int i,j,elem;
     SqStack S;
-	SqStack *pS;
-	S = InitStack(pS);
-    pS = &S;
-	printf("%d\n",pS->stacksize);
-	Push(pS,3);
-	int *i;
-	Pop(pS,i);
+	S = InitStack(&S);
+	printf("Empty? %d\n",EmptyStack(S));
+
+	for(j = 0 ; j < 20 ; j++){  
+        elem = rand()%100;  
+        Push(&S,elem);  
+    }
+	printf("Empty? %d\n",EmptyStack(S));
+    printf("Length = %d\n",LengthStack(S));
+
+    puts("\n");
+	BTPrintStack(S);
+    puts("\n");
+
+	Pop(&S,&i);
 	printf("%d\n",i);
+    puts("\n");
+
+	TBPrintStack(S);
+    puts("\n");
+
+	CleanStack(&S);
+	TBPrintStack(S);
+    puts("\n");
     return 0;
 }
