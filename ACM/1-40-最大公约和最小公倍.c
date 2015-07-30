fccc8b6d1113b33b13f040086816b8b6c6403d9c
Zty <361222163@qq.com>
2015-07-30 20:42:30 +0800

    update

---
+++ ACM/1-40-最大公约和最小公倍.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int gongyue(int a , int b){
+    int i = 1, c , j = 0 ,gy = 1;
+    c = a > b?a:b;
+	while ( i <= c ){
+        if (a / i * i == a && b / i * i ==b ){
+			gy = i ;
+        }
+		i++;
+	}
+	return gy;
+}
+
+int gongbei(int a , int b){
+    int i , j ,c;
+	c = a < b? b:a;
+	for ( i = 1;i <= b ;i++ ){
+		for (j = 1; j <= a ;j++ ){
+			if ( a * i == b * j){
+				return a*i;
+			}
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+    int n , a ,b ;
+	scanf("%d",&n);
+	while (n--){
+		scanf("%d %d",&a,&b);
+		printf("%d %d\n",gongyue(a,b),gongbei(a,b));
+	}
+    return 0;
+}
