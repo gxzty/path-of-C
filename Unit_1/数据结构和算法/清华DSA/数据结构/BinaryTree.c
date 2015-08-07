cefeed67738944f514c4af0bae424703fa3522d4
Zty <361222163@qq.com>
2015-08-07 23:36:58 +0800

    update

---
+++ Unit_1/数据结构和算法/清华DSA/数据结构/BinaryTree.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "define.c"
+
+#define ElemType int
+
+
+struct BTN{
+	ElemType date;
+	struct BTN *LifeChild;
+	struct BTN *RightChild;
+}BiTNode, *BiTree;
+
+status
+
+
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
