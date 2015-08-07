88bfa23d9615e4e080255bb0f930fbf5050c02c1
Zty <361222163@qq.com>
2015-08-07 23:55:20 +0800

    CreateBinaryTree

--- Unit_1/数据结构和算法/清华DSA/数据结构/BinaryTree.c
+++ Unit_1/数据结构和算法/清华DSA/数据结构/BinaryTree.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
+#include <malloc.h>
 #include "define.c"
 
 #define ElemType int
 
 
-struct BTN{
+typedef struct BTN{
 	ElemType date;
 	struct BTN *LifeChild;
 	struct BTN *RightChild;
-}BiTNode, *BiTree;
+}BTN, *BiTree;
 
-status
+Status CreateBiTree(BTN *T){
+	ElemType ch;
+	ch = getchar();
+    if (ch == ' '){
+        T = NULL;    
+    }
+	else {
+		if (!(T = (BTN *)malloc(sizeof(BTN)))) exit(OVERFLOW);
+	    T->date = ch;
+		CreateBiTree(T->LifeChild);
+		CreateBiTree(T->RightChild);
+	}
+	return OK;
+}
 
 
 int main(int argc, char *argv[])
 {
-	
+	BTN BT;
+	CreateBiTree(&BT);
 	return 0;
 }
