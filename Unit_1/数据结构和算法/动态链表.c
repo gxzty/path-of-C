1e3c3f907d7262b8da5f34761ccd160732d64d38
Zty <361222163@qq.com>
2015-08-13 00:00:54 +0800

    update

--- Unit_1/数据结构和算法/动态链表.c
+++ Unit_1/数据结构和算法/动态链表.c
@@ -16,7 +16,7 @@ struct people * Creat()
 	int n=0;
 	head = NULL;
 	p1=p2=(struct people*)malloc(sizeof(struct people));
-	printf("input id and age:\n");
+	printf("input id and age(like '1,15'),exit when id is '0':\n");
 	scanf("%d,%d",&p1->id,&p1->age);//scanf是格式化输入语句，输入数据方式要按照"1,2"的方式来输入！记得逗号！！
 	while (p1->id != 0){
 		n++;
